5dd450c2efc42b12013e8b815f0147ca5ac49315
Colby Aley <colby@aley.me>
2014-03-24 15:50:51 -0700

    Static controller

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,10 @@
+class StaticController < ApplicationController
+
+  def index
+  end
+
+  # This really needs to be put somewhere else.
+  def preferences
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 CoffeeTracker::Application.routes.draw do
   devise_for :users
 
-  root :to => 'drinks#index'
+  root :to => 'static#index'
   resources :drinks
+  get '/preferences', to: 'static#preferences'
 end
