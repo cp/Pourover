67b81547efd7f86be957a0b4c87d8255fdbb52bc
Colby Aley <colby@aley.me>
2014-05-03 22:53:03 -0700

    Don't load dotenv in prod

--- config/application.rb
+++ config/application.rb
@@ -3,7 +3,7 @@ require File.expand_path('../boot', __FILE__)
 require 'rails/all'
 require 'dotenv'
 
-Dotenv.load!
+Dotenv.load! unless Rails.env.production?
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
